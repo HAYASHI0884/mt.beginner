1362e07d7fc91dd5b58b2ea6ac04f150aa5170d6
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-02 17:20:26 +0900

    編集ページの作成

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,27 @@ class PagesController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      redirect_to pages_top_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to page_path(current_user.id)
+    else
+      render edit_page_path(current_user.id)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:text, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
   get "pages/top"
-  resources :pages
+  resources :pages, only:[:index, :show, :edit, :update]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
