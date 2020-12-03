20771d4ab3b751c4671a42c902d5f7fc5bfdef8c
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-03 21:12:46 +0900

    インスタンス変数の共通メソッド化

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_user, only:[:show, :edit, :update]
+
   def index
   end
 
@@ -6,16 +8,13 @@ class PagesController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
     redirect_to pages_top_path unless current_user.id == @user.id
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to page_path(current_user.id)
     else
@@ -27,5 +26,9 @@ class PagesController < ApplicationController
 
   def user_params
     params.require(:user).permit(:text, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
