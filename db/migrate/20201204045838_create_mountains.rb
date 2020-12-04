f0c2ff24e757c0cce003f36b3ef31532c64130d6
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-04 14:03:40 +0900

    マイグレーションファイルの作成

--- db/migrate/20201204045838_create_mountains.rb
+++ db/migrate/20201204045838_create_mountains.rb
@@ -1,6 +1,10 @@
 class CreateMountains < ActiveRecord::Migration[6.0]
   def change
     create_table :mountains do |t|
+      t.string  :name,       null: false
+      t.integer :area,       null: false
+      t.integer :elevation,  null: false
+      t.integer :climb_time, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_081005) do
+ActiveRecord::Schema.define(version: 2020_12_04_045838) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_12_02_081005) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "mountains", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "area", null: false
+    t.integer "elevation", null: false
+    t.integer "climb_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
