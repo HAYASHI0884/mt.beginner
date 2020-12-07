ec17195a1e7f4cbb047644113c421dfd3331d32e
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-07 23:02:10 +0900

    検索結果の表示

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,7 +1,19 @@
 class MountainsController < ApplicationController
   def index
+    @m = Mountain.ransack(params[:q])
+    @results = @m.result
+    set_column
   end
 
   def show
+    @mountain = Mountain.find(params[:id])
+  end
+
+  private
+
+  def set_column
+    @area = Area.select("name").distinct
+    @elevation = Elevation.select("name").distinct
+    @climb_time = ClimbTime.select("name").distinct
   end
 end
