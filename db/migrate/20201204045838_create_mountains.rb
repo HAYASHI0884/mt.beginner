395f02f0b52033f5822d9abe404d228b55c2d0e2
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-09 17:20:32 +0900

    マイグレーションファイルの修正

--- db/migrate/20201204045838_create_mountains.rb
+++ db/migrate/20201204045838_create_mountains.rb
@@ -1,7 +1,7 @@
 class CreateMountains < ActiveRecord::Migration[6.0]
   def change
     create_table :mountains do |t|
-      t.string  :name,       null: false
+      t.string     :name,       null: false
       t.references :area,       null: false
       t.references :elevation,  null: false
       t.references :climb_time, null: false
