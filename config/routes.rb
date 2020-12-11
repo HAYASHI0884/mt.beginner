69ce74d7026f637bed32945ea76906a622efd0b9
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-11 16:44:14 +0900

    投稿ページへのリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :tweets, only:[:show, :edit, :update, :create, :update, :destroy]
   devise_for :users
   root to: 'pages#index'
   get "pages/top"
-  resources :pages, only:[:index, :show, :edit, :update]
+  resources :pages, only:[:index, :show, :edit, :update] do
+    resources :tweets, only:[:show, :edit, :new, :create, :update, :destroy]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
