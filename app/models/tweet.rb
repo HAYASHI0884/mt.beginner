51ac06e23894266fae40c88b474468e8a9092821
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-15 17:13:43 +0900

    バリデーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,19 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :introduction, presence: true
+  validate :image_presence
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
+
 end
