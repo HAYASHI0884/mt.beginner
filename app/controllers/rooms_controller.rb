972b821ebeefb3d14b3c2f2c1cd73e61561e2969
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-17 22:00:28 +0900

    Roomsコントローラーの記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,32 @@
 class RoomsController < ApplicationController
+  def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to pages_top_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to pages_top_path
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
