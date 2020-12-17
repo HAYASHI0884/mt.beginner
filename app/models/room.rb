55154b4dc710f683066e94f19534f779c2ddb1e9
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-17 21:44:42 +0900

    写真添付機能の追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   validates :name,     presence: true
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_one_attached :image
 end
