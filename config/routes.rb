020dc5606416b4f1acde01c27dd37504699b619c
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-17 18:06:48 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     end
   end
   resources :tweets, only:[:show, :edit, :new, :create, :update, :destroy]
+  resources :messsages
+  resources :rooms
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
