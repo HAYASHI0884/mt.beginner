c4d08d454252695f3bbc2d05d1defd8c1cc2af6d
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-18 20:38:39 +0900

    アソシエーションの追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   validates :name, presence: true
 
-
+  belongs_to :user, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
   has_many :users, through: :entries, dependent: :destroy
