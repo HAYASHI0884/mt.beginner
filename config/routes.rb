b9b13bcd513a61b2793b95b8d2615bbb21043961
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-22 19:41:43 +0900

    誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
   resources :tweets, only:[:show, :edit, :new, :create, :update, :destroy]
   resources :rooms do
-    resources :messsages
+    resources :messages
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
