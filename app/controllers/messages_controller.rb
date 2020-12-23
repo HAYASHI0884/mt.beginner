d0b8885358a7d1b3a033483bb9219602f6331e5c
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-23 21:45:08 +0900

    @roomの共通メソッド化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,15 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room, only: [:index, :create]
 
   def index
     @message = Message.new
-    @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user, :room).order(id: 'DESC')
     @user = @room.users
     redirect_to pages_top_path unless @user.include?(current_user)
   end
 
   def create
-    @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
@@ -21,6 +20,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
 
   def message_params
     params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
