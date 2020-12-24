d826032c46498410531143f705ffa83840812987
HAYASHI0884 <lin.jiangda884@gmail.com>
2020-12-25 00:03:51 +0900

    記述の変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    if @room.presents?
+    if @room.present?
       @messages = @room.messages.includes(:user, :room).order(id: 'DESC')
       @user = @room.users
       redirect_to pages_top_path unless @user.include?(current_user)
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    if @room.presents?
+    if @room.present?
       @message = @room.messages.new(message_params)
       if @message.save
         redirect_to room_messages_path(@room)
