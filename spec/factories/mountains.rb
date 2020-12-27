1cd22b11f4e1de3b01aa6211d61e1d5891d171d2
林将大 <lin.jiangda884@gmail.com>
2020-12-27 21:53:41 +0900

    FactoryBotのアソシエーション追加

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :mountain do
+    association  :area
+    association  :elevation
+    association  :climb_time
     name          {"test"}
-    area_id       {1}
-    elevation_id  {1}
-    climb_time_id {1}
 
     after(:build) do |mountain|
       mountain.image.attach(io: File.open('app/assets/images/test_image.png'), filename: 'test_image.png')
