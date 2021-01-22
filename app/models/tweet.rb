a83d239c8c6f4864fb6fc12b1106f27fc2b2ff2c
林将大 <lin.jiangda884@gmail.com>
2021-01-22 23:35:38 +0900

    説明文の文量バリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   has_one_attached :image, dependent: :destroy
 
   validates :title, presence: true
-  validates :introduction, presence: true
+  validates :introduction, presence: true, length:{maximum:140}
   validate :image_presence
 
   def image_presence
