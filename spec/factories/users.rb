959390ed906a14c0811b7d14277a1691e53fab9f
林将大 <lin.jiangda884@gmail.com>
2021-01-25 20:33:47 +0900

    FactoryBot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,12 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {"000000"}
     password_confirmation {password}
-  end
-  factory :admin do
-    name                  {"admin"}
-    email                 {admin@mail.com}
-    password              {"11111111"}
-    password_confirmation {password}
+
+    factory :admin do
+      name                  {"admin"}
+      email                 {"admin@mail.com"}
+      password              {"11111111"}
+      password_confirmation {password}
+    end
   end
 end
