9f55f8fad449dfa8e3cd93908e7e8b54a64dbd86
林将大 <lin.jiangda884@gmail.com>
2021-02-05 22:30:39 +0900

    FactoryBotの名前変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name                  {"test"}
+    name                  {Faker::Name.name}
     email                 {Faker::Internet.free_email}
     password              {"000000"}
     password_confirmation {password}
