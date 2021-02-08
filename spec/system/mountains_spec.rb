cec255de37757b7b26cd9ae9be8c331a1b548c90
林将大 <lin.jiangda884@gmail.com>
2021-02-08 14:44:11 +0900

    山の投稿機能の結合テスト

--- spec/system/mountains_spec.rb
+++ spec/system/mountains_spec.rb
@@ -58,6 +58,62 @@ RSpec.describe "山の投稿", type: :system do
     @user = FactoryBot.create(:user)
     @admin = FactoryBot.create(:admin)
     @mountain = FactoryBot.build(:mountain)
+    @area = FactoryBot.create(:area)
+    @elevation = FactoryBot.create(:elevation)
+    @climb_time = FactoryBot.create(:climb_time)
+  end
+
+  context '山の投稿ができるとき' do
+    it '管理者は山の投稿を行う事ができる' do
+      # 管理者でログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @admin.email
+      fill_in 'パスワード', with: @admin.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq pages_top_path
+      # 管理者ページに遷移する
+      visit admin_mountains_path
+      # 投稿画面に遷移する
+      visit new_mountain_path
+      # フォームに情報を入力する
+      attach_file "mountain[image]", 'app/assets/images/test_image.png'
+      fill_in '山の名前', with: @mountain.name
+      select @area.name, from: "地域"
+      select @elevation.name, from: "標高"
+      select @climb_time.name, from: "総歩行時間"
+      # 送信するとMountainモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Mountain.count }.by(1)
+      # 管理者トップページに遷移することを確認する
+      expect(current_path).to eq admin_mountains_path
+      # 管理者トップページに先ほど投稿した山が存在することを確認する
+      expect(page).to have_content(@mountain.name)
+    end
+  end
+
+  context '山の投稿ができないとき' do
+    it '管理者以外のユーザーは山の投稿画面に遷移できない' do
+      # ログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq pages_top_path
+      # 管理者ページに遷移しようとする
+      visit admin_mountains_path
+      # topページへ遷移することを確認する
+      expect(current_path).to eq pages_top_path
+    end
+
+    it 'ログインしていないユーザーは山の投稿画面に遷移できない' do
+      # indexページに遷移する
+      visit root_path
+      # 管理者ページに遷移しようとする
+      visit admin_mountains_path
+      # ログイン画面に遷移した事を確認する
+      expect(current_path).to eq new_user_session_path
+    end
   end
 end
 
@@ -67,6 +123,22 @@ RSpec.describe "山の編集", type: :system do
     @admin = FactoryBot.create(:admin)
     @mountain = FactoryBot.create(:mountain)
   end
+
+  context '山の編集ができるとき' do
+    it '管理者は山の編集を行う事ができる' do
+
+    end
+  end
+
+  context '山の編集ができないとき' do
+    it '管理者以外のユーザーは山の編集画面に遷移できない' do
+
+    end
+
+    it 'ログインしていないユーザーは山の編集画面に遷移できない' do
+
+    end
+  end
 end
 
 RSpec.describe "山の削除", type: :system do
@@ -74,5 +146,11 @@ RSpec.describe "山の削除", type: :system do
     @user = FactoryBot.create(:user)
     @admin = FactoryBot.create(:admin)
     @mountain = FactoryBot.create(:mountain)
+  end
+
+  context '山の削除ができるとき' do
+    it '管理者は山の削除を行う事ができる' do
+
+    end
   end
 end
