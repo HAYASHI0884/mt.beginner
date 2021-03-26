aae3f11ad2893415d44197bd499ced7834c4dc97
林将大 <lin.jiangda884@gmail.com>
2021-03-26 23:50:38 +0900

    選択した画像情報の取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const ImageList = document.getElementById('image-list');
   document.getElementById('tweet_image').addEventListener('change', function (e) {
-    console.log(123);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
   });
 });
