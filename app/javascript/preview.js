c62cbc67b66465c8973c202b55fadd67082caf10
林将大 <lin.jiangda884@gmail.com>
2021-03-27 22:08:23 +0900

    newとedit画面のみJSを適用

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const ImageList = document.getElementById('image-list');
-  document.getElementById('tweet_image').addEventListener('change', function (e) {
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+if (document.URL.match(/new/) || document.URL.match(/edit/)) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const ImageList = document.getElementById('image-list');
+    document.getElementById('tweet_image').addEventListener('change', function (e) {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
   });
-});
+}
